b9e61dbc20c6db3f9de79722f11cd89ee89f38ea
serjio4433 <64637760+serjio4433@users.noreply.github.com>
2020-11-11 19:40:24 +0300

    feat:modal at js

--- RepairO/js/script.js
+++ RepairO/js/script.js
@@ -15,3 +15,19 @@ testWebP(function (support) {
   }
 });
 
+document.addEventListener("DOMContentLoaded", function (event) {
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
+
+
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
+  closeBtn.addEventListener('click', switchModal);
+});
+
+// });
